2f3dda9d689b4ec60b35e7be5151eaa59efc046f
Camilo-J <josecamilo1902@outlook.com>
2023-03-11 13:48:50 -0500

    Init Project

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.listen(port);
+
+console.log("Server on port", port);
