af2c194ca3a80cede123de8b354e4fe368b32f29
Camilo-J <josecamilo1902@outlook.com>
2023-03-12 19:29:10 -0500

    Add config for cloudinary

---
+++ app/config/cloudinary.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "DEV",
+  },
+});
+export { storage };
