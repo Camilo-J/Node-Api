f42e19b5f9459f92b25322511a9d482fe3a4c1c9
Camilo-J <josecamilo1902@outlook.com>
2023-03-20 17:50:41 -0500

    Add validation for tracjs

---
+++ app/validators/tracks.js
@@ -0,0 +1,24 @@
+import { check } from "express-validator";
+import { validateResults } from "../utils/handlerValidator.js";
+
+const validatorCreateItem = [
+  check("name").exists().notEmpty(),
+  check("album").exists().notEmpty(),
+  check("cover").exists().notEmpty(),
+  check("artist").exists().notEmpty(),
+  check("artist.name").exists().notEmpty(),
+  check("artist.nickname").exists().notEmpty(),
+  check("artist.nationality").exists().notEmpty(),
+  check("duration").exists().notEmpty(),
+  check("duration.start").exists().notEmpty(),
+  check("duration.end").exists().notEmpty(),
+  check("mediaId").exists().notEmpty().isMongoId(),
+  (req, res, next) => validateResults(req, res, next),
+];
+
+const validatorGetItem = [
+  check("id").exists().notEmpty().isMongoId(),
+  (req, res, next) => validateResults(req, res, next),
+];
+
+export { validatorCreateItem, validatorGetItem };
