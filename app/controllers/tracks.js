d0b648d050d388955603e432369480e5594e60a4
Camilo-J <josecamilo1902@outlook.com>
2023-03-20 19:01:56 -0500

    Add methods CRUD  in tracks controller

--- app/controllers/tracks.js
+++ app/controllers/tracks.js
@@ -1,20 +1,59 @@
+import { matchedData } from "express-validator";
 import models from "../models/index.js";
+import { handleHttpError } from "../utils/handleError.js";
 
 const getItems = async (req, res) => {
-  const data = await models.tracksModel.find({});
+  try {
+    const data = await models.tracksModel.find({});
 
-  res.json(data);
+    res.json(data);
+  } catch (e) {
+    handleHttpError(res, "Error_Get_Items");
+  }
 };
-const getItem = async (req, res) => {};
+const getItem = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const { id } = req;
+    const data = await models.tracksModel.findById(id);
+    console.log(req);
+    console.log(id);
+    res.json(data);
+  } catch (error) {
+    handleHttpError(res, "Error_Get_Item");
+  }
+};
 
 const createItem = async (req, res) => {
-  const { body } = req;
-  console.log(body);
-  const newItem = await models.tracksModel.create(body);
-  res.json(newItem);
+  try {
+    const body = matchedData(req);
+    const newItem = await models.tracksModel.create(body);
+    res.json(newItem);
+  } catch (e) {
+    handleHttpError(res, "Error_Post_Items");
+  }
 };
-const updateItem = async (req, res) => {};
 
-const deletetem = async (req, res) => {};
+const updateItem = async (req, res) => {
+  try {
+    const { id, ...body } = matchedData(req);
+    const newItem = await models.tracksModel.findOneAndUpdate(id, body);
+    res.json(newItem);
+  } catch (e) {
+    handleHttpError(res, "Error_Update_item");
+  }
+};
 
-export { getItems, getItem, createItem, updateItem, deletetem };
+const deleteItem = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const { id } = req;
+    const data = await models.tracksModel.deleteOne({ _id: id });
+
+    res.sendStatus(204);
+  } catch (error) {
+    handleHttpError(res, "Error_Delete_Item");
+  }
+};
+
+export { getItems, getItem, createItem, updateItem, deleteItem };
