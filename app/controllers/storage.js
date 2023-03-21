48e2e590a8bcc133208057051768a4e8d7924b05
Camilo-J <josecamilo1902@outlook.com>
2023-03-20 19:02:30 -0500

    [W.I.P] Add method CRUD for storage controller

--- app/controllers/storage.js
+++ app/controllers/storage.js
@@ -1,24 +1,53 @@
+import { matchedData } from "express-validator";
+import { cloudinary } from "../config/cloudinary.js";
 import models from "../models/index.js";
+import { handleHttpError } from "../utils/handleError.js";
 
 const getItems = async (req, res) => {
-  const data = await models.storageModel.find({});
-
-  res.json(data);
+  try {
+    const data = await models.storageModel.find({});
+    res.json(data);
+  } catch (error) {
+    handleHttpError(res, "Error_Get_Items");
+  }
 };
-const getItem = async (req, res) => {};
+
+const getItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req);
+    const data = await models.storageModel.findById(id);
+    res.json(data);
+  } catch (error) {
+    handleHttpError(res, "Error_Get_Item");
+  }
+};
 
 const createItem = async (req, res) => {
-  const { file } = req;
-  console.log(file);
-  const newdata = {
-    filename: file.filename,
-    url: file.path,
-  };
-  const newItem = await models.storageModel.create(newdata);
-  res.json(newItem);
+  try {
+    const { file } = req;
+    const newdata = {
+      filename: file.filename,
+      url: file.path,
+    };
+    const newItem = await models.storageModel.create(newdata);
+    res.json(newItem);
+  } catch (error) {
+    handleHttpError(res, "Error_Create_Item");
+  }
 };
-const updateItem = async (req, res) => {};
 
-const deletetem = async (req, res) => {};
+const deleteItem = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-export { getItems, getItem, createItem, updateItem, deletetem };
+    const media = await models.storageModel.deleteOne(id);
+    if (!media) res.status(404).json({ message: "File Not Found" });
+
+    await cloudinary.uploader.destroy(`${media.filename}`);
+    res.sendStatus(204);
+  } catch (error) {
+    handleHttpError(res, "Error_Delete_Item");
+  }
+};
+
+export { getItems, getItem, createItem, deleteItem };
