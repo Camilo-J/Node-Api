38cdce06339945e68ccc2af668ce7672ca2a144f
Camilo-J <josecamilo1902@outlook.com>
2023-03-20 19:04:23 -0500

    Add config to start server in app.js

--- app/app.js
+++ app/app.js
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import dbConnect from "./config/mongo.js";
+import router from "./routes/index.js";
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(cors());
 
-const port = process.env.PORT;
+app.use(express.json());
 
+app.use("/api/v1", router);
 app.listen(port);
 
 console.log("Server on port", port);
